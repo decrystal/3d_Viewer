13b9f8f888926c4b3187e9983177ca16be8352a1
decrystal <2399274254lsc@gmail.com>
2023-08-21 17:26:53 +0800

    feat: init

---
+++ debug/source/engine/threejs/threemodelloader.js
@@ -0,0 +1,163 @@
+import { Direction } from '../geometry/geometry.js';
+import { Importer } from '../import/importer.js';
+import { RevokeObjectUrl } from '../io/bufferutils.js';
+import { ConvertModelToThreeObject, ModelToThreeConversionOutput, ModelToThreeConversionParams } from './threeconverter.js';
+import { ConvertColorToThreeColor, HasHighpDriverIssue } from './threeutils.js';
+
+import * as THREE from 'three';
+
+
+// 构造函数：
+// constructor()：构造函数初始化了 ThreeModelLoader 类的实例。
+// 创建了一个 Importer 实例，用于导入模型。
+// 初始化了一些属性，包括 inProgress（表示加载是否正在进行）、defaultMaterial（默认材质）、objectUrls（用于存储对象的URL）和 hasHighpDriverIssue（检查是否存在高精度渲染问题）。
+// InProgress 方法：
+// InProgress()：返回当前加载是否正在进行的状态。
+// LoadModel 方法：
+// LoadModel(inputFiles, settings, callbacks)：加载模型的方法，接受输入文件、设置选项和回调函数。
+// 在加载过程中，首先检查是否有其他加载任务正在进行，如果有，则返回。
+// 设置 inProgress 为 true，表示加载正在进行中。
+// 调用 RevokeObjectUrls 方法，用于撤销之前创建的对象URL。
+// 调用 importer.ImportFiles 方法来导入输入文件，传递了一系列回调函数，用于处理不同加载阶段的操作。
+// 当导入成功后，将导入的模型转换为 Three.js 中的对象，并执行相应的回调函数。（ the key of the code）
+// GetImporter 方法：
+// GetImporter()：返回 Importer 实例，用于获取导入模型的引擎。
+// GetDefaultMaterial 和 ReplaceDefaultMaterialColor 方法：
+// GetDefaultMaterial()：返回默认材质。
+// ReplaceDefaultMaterialColor(defaultColor)：替换默认材质的颜色，如果默认材质存在且不使用顶点颜色。
+// RevokeObjectUrls 方法：
+// RevokeObjectUrls()：撤销之前创建的对象URL，防止内存泄漏。遍历 objectUrls 数组，使用 RevokeObjectUrl 方法来撤销URL。
+// Destroy 方法：
+// Destroy()：销毁方法，用于撤销对象URL并将 importer 设置为 null。
+// 综上所述，ThreeModelLoader 类封装了模型的加载、转换和管理操作，提供了一些实用的方法来获取导入引擎、默认材质，以及处理对象URL的管理和销毁。它在加载和处理模型时与外部代码通过回调函数进行交互，以确保加载和显示模型的顺利进行。
+
+export class ThreeModelLoader
+{
+    constructor ()
+    {
+        this.importer = new Importer ();
+        this.inProgress = false;
+        this.defaultMaterial = null;
+        this.objectUrls = null;
+        this.hasHighpDriverIssue = HasHighpDriverIssue ();
+    }
+
+    InProgress ()
+    {
+        return this.inProgress;
+    }
+
+    LoadModel (inputFiles, settings, callbacks)
+    {
+        if (this.inProgress) {
+            return;
+        }
+
+        this.inProgress = true;
+        this.RevokeObjectUrls ();
+        this.importer.ImportFiles (inputFiles, settings, {
+            onLoadStart : () => {
+                callbacks.onLoadStart ();
+            },
+            onFileListProgress : (current, total) => {
+                callbacks.onFileListProgress (current, total);
+            },
+            onFileLoadProgress : (current, total) => {
+                callbacks.onFileLoadProgress (current, total);
+            },
+            onImportStart : () => {
+                callbacks.onImportStart ();
+            },
+            onSelectMainFile : (fileNames, selectFile) => {
+                if (!callbacks.onSelectMainFile) {
+                    selectFile (0);
+                } else {
+                    callbacks.onSelectMainFile (fileNames, selectFile);
+                }
+            },
+            onImportSuccess : (importResult) => {
+
+                // 这段代码块的作用是根据导入结果中的 upVector 属性来调整三维模型的方向，并在处理完模型后触发相应的回调函数。我会一步步解释每个部分的含义：
+                // this.defaultMaterial = output.defaultMaterial;
+                // 将输出中的 defaultMaterial 赋值给类实例的 defaultMaterial 属性。这可能是用来管理默认材质的属性。
+                // this.objectUrls = output.objectUrls;
+                // 将输出中的 objectUrls 赋值给类实例的 objectUrls 属性。这是用来存储对象URL的数组，用于后续的撤销操作。
+                // 条件语句根据 importResult.upVector 的值来确定旋转方向：
+                // if (importResult.upVector === Direction.X)：如果 upVector 是 Direction.X，表示模型的上方向是X轴方向。
+                // else if (importResult.upVector === Direction.Z)：如果 upVector 是 Direction.Z，表示模型的上方向是Z轴方向。
+                // 旋转模型的方向：
+                // 使用 THREE.Quaternion 来创建一个四元数，setFromAxisAngle 方法将旋转轴和旋转角度（这里是 Math.PI / 2.0 或 -Math.PI / 2.0）转换为四元数表示。
+                // new THREE.Vector3 (0.0, 0.0, 1.0) 表示旋转轴，这是一个指向Z轴的单位向量（用于X轴方向的旋转）。
+                // new THREE.Vector3 (1.0, 0.0, 0.0) 表示旋转轴，这是一个指向X轴的单位向量（用于Z轴方向的旋转）。
+                // threeObject.quaternion.multiply(rotation); 旋转 threeObject 的四元数（表示模型的旋转）。
+                // 触发回调函数：
+                // callbacks.onModelFinished(importResult, threeObject); 调用传递的 onModelFinished 回调函数，传入导入结果 importResult 和经过旋转的 threeObject。
+                // this.inProgress = false; 设置 inProgress 属性为 false，表示加载过程结束。
+
+                callbacks.onVisualizationStart ();
+                // 将导入的模型转换为 Three.js 中的对象，并执行相应的回调函数。
+                let params = new ModelToThreeConversionParams ();
+                params.forceMediumpForMaterials = this.hasHighpDriverIssue;
+                let output = new ModelToThreeConversionOutput ();
+                console.log('test-new ModelToThreeConversionParams ()', params, '\n', 'test-importResult', importResult.model, '\n', 'test-output', output);
+
+                ConvertModelToThreeObject (importResult.model, params, output, {
+                    onTextureLoaded : () => {
+                        callbacks.onTextureLoaded ();
+                    },
+                    onModelLoaded : (threeObject) => {
+                        this.defaultMaterial = output.defaultMaterial;
+                        this.objectUrls = output.objectUrls;
+                        if (importResult.upVector === Direction.X) {
+                            let rotation = new THREE.Quaternion ().setFromAxisAngle (new THREE.Vector3 (0.0, 0.0, 1.0), Math.PI / 2.0);
+                            threeObject.quaternion.multiply (rotation);
+                        } else if (importResult.upVector === Direction.Z) {
+                            let rotation = new THREE.Quaternion ().setFromAxisAngle (new THREE.Vector3 (1.0, 0.0, 0.0), -Math.PI / 2.0);
+                            threeObject.quaternion.multiply (rotation);
+                        }
+                        callbacks.onModelFinished (importResult, threeObject);
+                        this.inProgress = false;
+                    }
+                });
+            },
+            onImportError : (importError) => {
+                callbacks.onLoadError (importError);
+                this.inProgress = false;
+            }
+        });
+    }
+
+    GetImporter ()
+    {
+        return this.importer;
+    }
+
+    GetDefaultMaterial ()
+    {
+        return this.defaultMaterial;
+    }
+
+    ReplaceDefaultMaterialColor (defaultColor)
+    {
+        if (this.defaultMaterial !== null && !this.defaultMaterial.vertexColors) {
+            this.defaultMaterial.color = ConvertColorToThreeColor (defaultColor);
+        }
+    }
+
+    RevokeObjectUrls ()
+    {
+        if (this.objectUrls === null) {
+            return;
+        }
+        for (let objectUrl of this.objectUrls) {
+            RevokeObjectUrl (objectUrl);
+        }
+        this.objectUrls = null;
+    }
+
+    Destroy ()
+    {
+        this.RevokeObjectUrls ();
+        this.importer = null;
+    }
+}
---
+++ debug/source/website/website.js
@@ -0,0 +1,395 @@
+import { InputFilesFromFileObjects, InputFilesFromUrls } from '../engine/import/importerfiles.js';
+import { ImportErrorCode, ImportSettings } from '../engine/import/importer.js';
+import { Viewer } from '../engine/viewer/viewer.js';
+import { AddDiv, AddDomElement, ShowDomElement, SetDomElementOuterHeight, CreateDomElement, GetDomElementOuterWidth, GetDomElementOuterHeight } from '../engine/viewer/domutils.js';
+import { CalculatePopupPositionToScreen, ShowListPopup } from './dialogs.js';
+import { HandleEvent } from './eventhandler.js';
+import { Settings, Theme } from './settings.js';
+import { ThreeModelLoaderUI } from './threemodelloaderui.js';
+import { Toolbar } from './toolbar.js';
+import { IsSmallWidth } from './utils.js';
+
+import { Direction } from '../engine/geometry/geometry.js';
+
+import { EnumeratePlugins, PluginType } from './pluginregistry.js';
+
+// this is debugger point
+const WebsiteUIState =
+{
+    Undefined : 0,
+    Intro : 1,
+    Model : 2,
+    Loading : 3
+};
+
+class WebsiteLayouter
+{
+    constructor (parameters, navigator, sidebar, viewer, measureTool)
+    {
+        this.parameters = parameters;
+        this.navigator = navigator;
+        this.sidebar = sidebar;
+        this.viewer = viewer;
+        this.measureTool = measureTool;
+        this.limits = {
+            minPanelWidth : 290,
+            minCanvasWidth : 100
+        };
+    }
+
+    Init ()
+    {
+        this.InstallSplitter (this.parameters.navigatorSplitterDiv, this.parameters.navigatorDiv, (originalWidth, xDiff) => {
+            let newWidth = originalWidth + xDiff;
+            this.OnSplitterDragged (newWidth - this.navigator.GetWidth (), 0);
+        });
+
+        this.InstallSplitter (this.parameters.sidebarSplitterDiv, this.parameters.sidebarDiv, (originalWidth, xDiff) => {
+            let newWidth = originalWidth - xDiff;
+            this.OnSplitterDragged (0, newWidth - this.sidebar.GetWidth ());
+        });
+
+        this.Resize ();
+    }
+
+    InstallSplitter (splitterDiv, resizedDiv, onSplit)
+    {
+        let originalWidth = null;
+        // CreateVerticalSplitter (splitterDiv, {
+        //     onSplitStart : () => {
+        //         originalWidth = GetDomElementOuterWidth (resizedDiv);
+        //     },
+        //     onSplit : (xDiff) => {
+        //         onSplit (originalWidth, xDiff);
+        //     }
+        // });
+    }
+
+    OnSplitterDragged (leftDiff, rightDiff)
+    {
+        let windowWidth = window.innerWidth;
+
+        let navigatorWidth = this.navigator.GetWidth ();
+        let sidebarWidth = this.sidebar.GetWidth ();
+
+        let leftWidth = GetDomElementOuterWidth (this.parameters.leftContainerDiv);
+        let rightWidth = GetDomElementOuterWidth (this.parameters.rightContainerDiv);
+
+        let newLeftWidth = leftWidth + leftDiff;
+        let newRightWidth = rightWidth + rightDiff;
+        let contentNewWidth = windowWidth - newLeftWidth - newRightWidth;
+
+        let isNavigatorVisible = this.navigator.IsPanelsVisible ();
+        let isSidebarVisible = this.sidebar.IsPanelsVisible ();
+
+        if (isNavigatorVisible && newLeftWidth < this.limits.minPanelWidth) {
+            newLeftWidth = this.limits.minPanelWidth;
+        }
+
+        if (isSidebarVisible && newRightWidth < this.limits.minPanelWidth) {
+            newRightWidth = this.limits.minPanelWidth;
+        }
+
+        if (contentNewWidth < this.limits.minCanvasWidth) {
+            if (leftDiff > 0) {
+                newLeftWidth = windowWidth - newRightWidth - this.limits.minCanvasWidth;
+            } else if (rightDiff > 0) {
+                newRightWidth = windowWidth - newLeftWidth - this.limits.minCanvasWidth;
+            }
+        }
+
+        if (isNavigatorVisible) {
+            let newNavigatorWidth = navigatorWidth + (newLeftWidth - leftWidth);
+            this.navigator.SetWidth (newNavigatorWidth);
+        }
+        if (isSidebarVisible) {
+            let newSidebarWidth = sidebarWidth + (newRightWidth - rightWidth);
+            this.sidebar.SetWidth (newSidebarWidth);
+        }
+
+        this.Resize ();
+    }
+
+    Resize ()
+    {
+        let windowWidth = window.innerWidth;
+        let windowHeight = window.innerHeight;
+        let headerHeight = this.parameters.headerDiv.offsetHeight;
+
+        let leftWidth = 0;
+        let rightWidth = 0;
+        let safetyMargin = 0;
+        if (!IsSmallWidth ()) {
+            leftWidth = GetDomElementOuterWidth (this.parameters.leftContainerDiv);
+            rightWidth = GetDomElementOuterWidth (this.parameters.rightContainerDiv);
+            safetyMargin = 1;
+        }
+
+        let contentWidth = windowWidth - leftWidth - rightWidth;
+        let contentHeight = windowHeight - headerHeight;
+
+        if (contentWidth < this.limits.minCanvasWidth) {
+            let neededIncrease = this.limits.minCanvasWidth - contentWidth;
+
+            let isNavigatorVisible = this.navigator.IsPanelsVisible ();
+            let isSidebarVisible = this.sidebar.IsPanelsVisible ();
+
+            if (neededIncrease > 0 && isNavigatorVisible) {
+                let navigatorDecrease = Math.min (neededIncrease, leftWidth - this.limits.minPanelWidth);
+                this.navigator.SetWidth (this.navigator.GetWidth () - navigatorDecrease);
+                neededIncrease = neededIncrease - navigatorDecrease;
+            }
+
+            if (neededIncrease > 0 && isSidebarVisible) {
+                let sidebarDecrease = Math.min (neededIncrease, rightWidth - this.limits.minPanelWidth);
+                this.sidebar.SetWidth (this.sidebar.GetWidth () - sidebarDecrease);
+            }
+
+            leftWidth = GetDomElementOuterWidth (this.parameters.leftContainerDiv);
+            rightWidth = GetDomElementOuterWidth (this.parameters.rightContainerDiv);
+            contentWidth = windowWidth - leftWidth - rightWidth;
+        }
+
+        // this.navigator.Resize (contentHeight);
+        SetDomElementOuterHeight (this.parameters.navigatorSplitterDiv, contentHeight);
+
+        // this.sidebar.Resize (contentHeight);
+        SetDomElementOuterHeight (this.parameters.sidebarSplitterDiv, contentHeight);
+
+        SetDomElementOuterHeight (this.parameters.introDiv, contentHeight);
+        this.viewer.Resize (contentWidth - safetyMargin, contentHeight);
+
+        let introContentHeight = GetDomElementOuterHeight (this.parameters.introContentDiv);
+        let introContentTop = (contentHeight - introContentHeight) / 3.0;
+        this.parameters.introContentDiv.style.top = introContentTop.toString () + 'px';
+
+        // this.measureTool.Resize ();
+    }
+}
+
+export class Website
+{
+    constructor (parameters)
+    {   console.log('test-parameters,', parameters);
+        this.parameters = parameters;
+        this.settings = new Settings (Theme.Light);
+        this.viewer = new Viewer (); // 模型展示的Viwer
+        this.toolbar = new Toolbar (this.parameters.toolbarDiv);
+        this.modelLoaderUI = new ThreeModelLoaderUI ();
+        this.uiState = WebsiteUIState.Undefined;
+        this.layouter = new WebsiteLayouter (this.parameters, {}, {}, this.viewer, {});
+        this.model = null;
+    }
+
+    Load ()
+    {
+        this.settings.LoadFromCookies ();
+        HandleEvent ('theme_on_load', this.settings.themeId === Theme.Light ? 'light' : 'dark');
+
+        EnumeratePlugins (PluginType.Header, (plugin) => {
+            plugin.registerButtons ({
+                createHeaderButton : (icon, title, link) => {
+                    // this.CreateHeaderButton (icon, title, link);
+                }
+            });
+        });
+
+        this.InitViewer ();
+        this.InitToolbar ();
+
+        this.viewer.SetMouseClickHandler (this.OnModelClicked.bind (this));
+        this.viewer.SetMouseMoveHandler (this.OnModelMouseMoved.bind (this));
+        this.viewer.SetContextMenuHandler (this.OnModelContextMenu.bind (this));
+
+        this.layouter.Init ();
+        this.SetUIState (WebsiteUIState.Intro);
+
+        window.addEventListener ('resize', () => {
+			this.layouter.Resize ();
+		});
+    }
+
+    SetUIState (uiState)
+    {
+        function ShowOnlyOnModelElements (show)
+        {
+            let root = document.querySelector (':root');
+            root.style.setProperty ('--ov_only_on_model_display', show ? 'inherit' : 'none');
+        }
+
+        if (this.uiState === uiState) {
+            return;
+        }
+
+        this.uiState = uiState;
+        if (this.uiState === WebsiteUIState.Intro) {
+            ShowDomElement (this.parameters.introDiv, true);
+            ShowDomElement (this.parameters.headerDiv, true);
+            ShowDomElement (this.parameters.mainDiv, false);
+            ShowOnlyOnModelElements (false);
+        } else if (this.uiState === WebsiteUIState.Model) {
+            ShowDomElement (this.parameters.introDiv, false);
+            ShowDomElement (this.parameters.headerDiv, true);
+            ShowDomElement (this.parameters.mainDiv, true);
+            ShowOnlyOnModelElements (true);
+            // this.UpdatePanelsVisibility ();
+        } else if (this.uiState === WebsiteUIState.Loading) {
+            ShowDomElement (this.parameters.introDiv, false);
+            ShowDomElement (this.parameters.headerDiv, true);
+            ShowDomElement (this.parameters.mainDiv, false);
+            ShowOnlyOnModelElements (false);
+        }
+
+        this.layouter.Resize ();
+    }
+
+    OnModelLoaded (importResult, threeObject)
+    {
+        this.model = importResult.model;
+        this.parameters.fileNameDiv.innerHTML = importResult.mainFile;
+        this.viewer.SetMainObject (threeObject);
+        this.viewer.SetUpVector (Direction.Y, false);
+    }
+
+    OnModelClicked (button, mouseCoordinates)
+    {
+        if (button !== 1) {
+            return;
+        }
+
+        let meshUserData = this.viewer.GetMeshUserDataUnderMouse (mouseCoordinates);
+        if (meshUserData === null) {
+            // this.navigator.SetSelection (null);
+        } else {
+            // this.navigator.SetSelection (new Selection (SelectionType.Mesh, meshUserData.originalMeshInstance.id));
+        }
+    }
+
+    OnModelMouseMoved (mouseCoordinates)
+    {
+
+    }
+
+    OnModelContextMenu (globalMouseCoordinates, mouseCoordinates)
+    {
+        let meshUserData = this.viewer.GetMeshUserDataUnderMouse (mouseCoordinates);
+        let items = [];
+        if (meshUserData === null) {
+            items.push ({
+                name : 'Fit model to window',
+                icon : 'fit',
+                onClick : () => {
+                    // this.FitModelToWindow (false);
+                }
+            });
+
+        } else {
+            items.push ({
+                name : 'Hide mesh',
+                icon : 'hidden',
+                onClick : () => {
+                    // this.navigator.ToggleMeshVisibility (meshUserData.originalMeshInstance.id);
+                }
+            });
+            items.push ({
+                name : 'Fit mesh to window',
+                icon : 'fit',
+                onClick : () => {
+                    // this.navigator.FitMeshToWindow (meshUserData.originalMeshInstance.id);
+                }
+            });
+        }
+        ShowListPopup (items, {
+            calculatePosition : (contentDiv) => {
+                return CalculatePopupPositionToScreen (globalMouseCoordinates, contentDiv);
+            },
+            onClick : (index) => {
+                let clickedItem = items[index];
+                clickedItem.onClick ();
+            }
+        });
+    }
+
+    OpenFileBrowserDialog ()
+    {
+        this.parameters.fileInput.click ();
+    }
+
+    LoadModelFromUrlList (urls, settings)
+    {
+        let inputFiles = InputFilesFromUrls (urls);
+        this.LoadModelFromInputFiles (inputFiles, settings);
+        // this.ClearHashIfNotOnlyUrlList ();
+    }
+
+    LoadModelFromFileList (files)
+    {
+        let importSettings = new ImportSettings ();
+        importSettings.defaultColor = this.settings.defaultColor;
+        let inputFiles = InputFilesFromFileObjects (files);
+        this.LoadModelFromInputFiles (inputFiles, importSettings);
+        this.ClearHashIfNotOnlyUrlList ();
+    }
+
+    LoadModelFromInputFiles (files, settings)
+    {
+        console.log('test-modelLoaderUI', this.modelLoaderUI);
+        this.modelLoaderUI.LoadModel (files, settings, {
+            onStart : () =>
+            {
+                this.SetUIState (WebsiteUIState.Loading);
+                // this.ClearModel ();
+            },
+            onFinish : (importResult, threeObject) =>
+            {
+                // this is end of analyzing the model
+                console.log('test-model-finished', importResult, threeObject);
+                this.SetUIState (WebsiteUIState.Model);
+                this.OnModelLoaded (importResult, threeObject);
+                // let importedExtension = GetFileExtension (importResult.mainFile);
+                // HandleEvent ('model_loaded', importedExtension);
+            },
+            onRender : () =>
+            {
+                // this.viewer.Render ();
+            },
+            onError : (importError) =>
+            {
+
+            }
+        });
+    }
+
+    InitViewer ()
+    {
+        let canvas = AddDomElement (this.parameters.viewerDiv, 'canvas');
+        this.viewer.Init (canvas);
+    }
+
+    InitToolbar ()
+    {
+        function AddButton (toolbar, imageName, imageTitle, classNames, onClick)
+        {
+            let button = toolbar.AddImageButton (imageName, imageTitle, () => {
+                onClick ();
+            });
+            for (let className of classNames) {
+                button.AddClass (className);
+            }
+            return button;
+        }
+
+        AddButton (this.toolbar, 'open', 'Open from your device', [], () => {
+            this.OpenFileBrowserDialog ();
+        });
+
+        this.parameters.fileInput.addEventListener ('change', (ev) => {
+
+            if (ev.target.files.length > 0) {
+                HandleEvent ('model_load_started', 'open_file');
+                console.log('test-model-input', ev.target.files);
+                this.LoadModelFromFileList (ev.target.files);
+            }
+        });
+    }
+}
